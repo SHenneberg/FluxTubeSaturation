f3af388a62e2dab8ddae6c70da245a20a439c2c4
SHenneberg <Sophia.Henneberg@gmail.com>
2022-09-07 11:46:41 +0200

    created FieldLineEquilibrium.m

---
+++ tools/FieldLineEquilibrium.m
@@ -0,0 +1,8 @@
+% Algorithm which solves the shape of an ellipctical flux tube
+% in equilibrium 
+% given by the PDE in "Explosive Instability and Erupting
+% Flux Tubes in a Magnetised Plasma Atmosphere" by Cowley, Cowley, 
+% Henneberg, Wilson, arXiv:1411.7797v1,  Eq. (54) 
+%
+% input parameters are: x_rho, x_B, A, B1, B2, q, x0
+
