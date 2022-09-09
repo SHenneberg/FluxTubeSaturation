27cfb28fe74f9d5f3e3346a24274016f91199a46
SHenneberg <Sophia.Henneberg@gmail.com>
2022-09-10 00:04:00 +0200

    Implemented Shooting Method in the FieldLineEquilibrium solver.

--- tools/FieldLineEquilibrium.m
+++ tools/FieldLineEquilibrium.m
@@ -20,22 +20,41 @@ B1 = 3;
 B2 = 1.8;
 %q=3.25;
 %q=4.5;
-q=7.23;
+q=4.23;
 %q = 25.25;
 
 h=0.01;
-zmax=1.;
+zmax=1.; %DON'T CHANGE this anymore!! It's important for the root finding algorithm
 N=zmax/h;
-z0=0;
+z0=0; %DON'T CHANGE this anymore!! It's important for the root finding algorithm
+%input for root finding
+qinit1=q+1;
+qinit2=q-1;
+itr=20;
+tol=10^(-8);
+
+
+xsym=@(qvar) xsymmetry(xrho,xB,A,B1,B2,z0,qvar,x0,h,N);
+
+
+[qroot,success,ifinal] = SecantMethod(xsym,qinit1,qinit2,itr,tol);
+
+qroot
+ifinal
 
 % second order ODE:
-[zgritQ,xequilQ]=FieldLineEquilibriumStep2(xrho,xB,A,B1,B2,q,z0,x0,h,N);
+[zgritQ,xequilQ]=FieldLineEquilibriumStep2(xrho,xB,A,B1,B2,qroot,z0,x0,h,N);
 plot(zgritQ,xequilQ)
 
 % first order ODE:
 %[zgritQ1,xequilQ1]=FieldLineEquilibriumStep(xrho,xB,A,B1,B2,q,z0,x0,h,N);
 %plot(zgritQ1,xequilQ1,'--',zgritQ,xequilQ)
 
-
+% function which need to be 0 when the right q is found:
+% symmetry of x with respect to z=0.5
+function[xsym]=xsymmetry(xrho,xB,A,B1,B2,z0,q,x0,h,N)
+    [zgritQ,xsymvar]=FieldLineEquilibriumStep2(xrho,xB,A,B1,B2,q,z0,x0,h,N);
+    xsym=xsymvar(1)-xsymvar(N+1);    
+end
 
 
