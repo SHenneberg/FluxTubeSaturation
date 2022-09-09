2bcb347ec37bb16205480816aa8d6d6b4b2712d0
SHenneberg <Sophia.Henneberg@gmail.com>
2022-09-09 16:25:22 +0200

    Edited some variable names for clarity.

--- tools/FieldLineEquilibrium.m
+++ tools/FieldLineEquilibrium.m
@@ -5,23 +5,31 @@
 % Henneberg, Wilson, arXiv:1411.7797v1,  Eq. (54) 
 %
 % input parameters are: x_rho, x_B, A, B1, B2, q, x0
+clear all
+close all
 
-% default input parameters:
+% input parameters:
 A = 0.161604;
-x0 = 0.5;
+%x0 = 1.2;
+%x0=1.1;
+x0=1.0;
+%x0=0.5;
 xrho = 2;
 xB = 0.8;
 B1 = 3;
 B2 = 1.8;
-q = 3.25;
+%q=3.25;
+%q=4.5;
+q=6.15;
+%q = 25.25;
 
-h=0.005;
+h=0.05;
 zmax=0.5;
 N=zmax/h;
 z0=0;
 
-[zgrit,xequil]=FieldLineEquilibriumStep(xrho,xB,A,B1,B2,q,z0,x0,h,N);
-plot(zgrit,xequil)
+[zgritQ,xequilQ]=FieldLineEquilibriumStep(xrho,xB,A,B1,B2,q,z0,x0,h,N);
+plot(zgritQ,xequilQ)
 
 
 
